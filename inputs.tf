fdba78ba997c62ecc99b627f6aa5c90fe0998e00
varleg <johninge@varleg.no>
2022-01-17 10:23:26 +0100

    test

--- inputs.tf
+++ inputs.tf
@@ -7,25 +7,14 @@ variable "ARM_CLIENT_SECRET" {
 variable "ARM_CLIENT_ID" {
   type        = string
   description = "Azure Service Principal ID"
-
 }
 
 variable "ARM_TENANT_ID" {
   type        = string
   description = "TenantId of the Azure AD Tenant"
-
-  validation {
-    condition     = can(regex("^[a-f0-9-]{36}$", var.tenant_id))
-    error_message = "The tenant_id does not follow the format ffffffff-ffff-ffff-ffff-ffffffffffff."
-  }
 }
 
 variable "ARM_SUBSCRIPTION_ID" {
   type        = string
   description = "Subscription ID of the target Azure Subscription"
-
-  validation {
-    condition     = can(regex("^[a-f0-9-]{36}$", var.subscription_id))
-    error_message = "The subscription_id does not follow the format ffffffff-ffff-ffff-ffff-ffffffffffff."
-  }
 }
